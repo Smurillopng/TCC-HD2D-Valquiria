d2a8e8ab44f321fca0632dd6888265f97eacc6f6
Sergio <smurillopng@gmail.com>
2023-04-18 20:00:06 -0300

    Camera ocultando objetos

---
+++ Assets/TCCHD2D/Code/Scripts/Player/DistanceHider.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+    public class DistanceHider : MonoBehaviour
+    {
+            public Transform player;
+            public LayerMask layerMask;
+            public Camera mainCam;
+
+            private HashSet<GameObject> hiddenObjects = new HashSet<GameObject>();
+            
+            private void Awake()
+            {
+                mainCam = Camera.main;
+            }
+
+            void Update()
+            {
+                RaycastHit[] hits;
+                Vector3 direction = player.position - mainCam.transform.position;
+                hits = Physics.RaycastAll(mainCam.transform.position, direction, direction.magnitude, layerMask);
+
+                // Hide newly hit objects
+                foreach (RaycastHit hit in hits)
+                {
+                    GameObject hitObject = hit.collider.gameObject;
+                    var hitRender = hitObject.GetComponent<Renderer>();
+                    var alphaValue = hitRender.material.GetFloat("_alphaValue");
+                    if (!hiddenObjects.Contains(hitObject))
+                    {
+                        hitRender.material.SetFloat("_alphaValue", 0.1f);
+                        hiddenObjects.Add(hitObject);
+                    }
+                }
+
+                // Show objects that are no longer being hit
+                hiddenObjects.RemoveWhere(obj =>
+                {
+                    if (!Physics.Raycast(mainCam.transform.position, direction, out RaycastHit hitInfo, direction.magnitude, layerMask) || hitInfo.collider.gameObject != obj)
+                    {
+                        var hitRender = obj.GetComponent<Renderer>();
+                        var alphaValue = hitRender.material.GetFloat("_alphaValue");
+                        hitRender.material.SetFloat("_alphaValue", 1);
+                        return true;
+                    }
+                    return false;
+                });
+            }
+    }

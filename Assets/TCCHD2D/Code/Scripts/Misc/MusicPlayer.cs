e8e123876d141c28671f9a1e70ac96bce393f3d6
Sergio <smurillopng@gmail.com>
2023-04-23 20:08:30 -0300

    NPCs e items pt.2

--- Assets/TCCHD2D/Code/Scripts/Misc/MusicPlayer.cs
+++ Assets/TCCHD2D/Code/Scripts/Misc/MusicPlayer.cs
@@ -1,9 +1,35 @@
-﻿using UnityEngine;
+﻿using System;
+using MuriPNG.Audio;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 
-namespace TCCHD2D.Code.Scripts.Misc
+public class MusicPlayer : MonoBehaviour
 {
-    public class MusicPlayer : MonoBehaviour
+    private void Start()
     {
-        
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    private void OnSceneLoaded(Scene arg0, LoadSceneMode arg1)
+    {
+        var sceneType = PlayerControls.Instance.SceneMap[SceneManager.GetActiveScene().name];
+        SoundManager.Instance.StopAllSounds();
+        switch (sceneType)
+        {
+            case SceneType.Game:
+                SoundManager.Instance.PlaySound("gameMusic");
+                break;
+            case SceneType.Menu:
+                SoundManager.Instance.PlaySound("menuMusic");
+                break;
+            case SceneType.Combat:
+                SoundManager.Instance.PlaySound("combatMusic");
+                break;
+        }
+    }
+
+    private void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
     }
 }

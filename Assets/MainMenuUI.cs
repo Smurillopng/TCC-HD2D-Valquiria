d1f45fb4acbc62da9a7c126eca23fae11769e7c0
Sergio <smurillopng@gmail.com>
2023-05-18 21:57:57 -0300

    0.1

---
+++ Assets/MainMenuUI.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuUI : MonoBehaviour
+{
+    public Image pressButtonImg;
+    public GameObject buttonsContainer;
+    public float time = 1f;
+    public float length = 1f;
+    public bool anyButtonPressed;
+    
+    private void Update()
+    {
+        if (anyButtonPressed)
+        {
+            pressButtonImg.gameObject.SetActive(false);
+            buttonsContainer.SetActive(true);
+            return;
+        }
+        WaitingForInput();
+    }
+
+    private void WaitingForInput()
+    {
+        if (!anyButtonPressed)
+        {
+            var color = pressButtonImg.color;
+            color.a = Mathf.Lerp(0, 1, Mathf.PingPong(Time.time / time, length));
+            pressButtonImg.color = color;
+        }
+        if (Input.anyKeyDown)
+        {
+            anyButtonPressed = true;
+        }
+    }
+}

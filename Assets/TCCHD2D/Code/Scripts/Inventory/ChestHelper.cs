333e70d6d4562414012173166b7b3d3db136ecc5
Sergio <smurillopng@gmail.com>
2023-08-28 21:17:06 -0300

    Resolvendo animação do baú
    Arrumando problemas de animação relacionados a estado no save

---
+++ Assets/TCCHD2D/Code/Scripts/Inventory/ChestHelper.cs
@@ -0,0 +1,22 @@
+﻿// Created by Sérgio Murillo da Costa Faria
+// Date: 28/08/2023
+
+using CI.QuickSave;
+using UnityEngine;
+
+public class ChestHelper : MonoBehaviour
+{
+    private Animator _animator;
+
+    private void Awake()
+    {
+        _animator = GetComponentInParent<Animator>();
+    }
+
+    public void CheckSave()
+    {
+        if (!QuickSaveReader.Create("GameSave").Exists($"{name}")) return;
+        var saveData = QuickSaveReader.Create("GameSave");
+        _animator.Play(saveData.Read<bool>($"{name}") ? "Chest Opened" : "Chest Closed");
+    }
+}

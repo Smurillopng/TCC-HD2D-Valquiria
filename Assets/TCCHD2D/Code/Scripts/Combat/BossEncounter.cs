527372c741c24b9c296fe80bf5d10c98914bb319
Sérgio Murillo <sergiomurillof@gmail.com>
2023-10-24 22:21:51 -0300

    Fix: tochas e bosses

--- Assets/TCCHD2D/Code/Scripts/Combat/BossEncounter.cs
+++ Assets/TCCHD2D/Code/Scripts/Combat/BossEncounter.cs
@@ -7,27 +7,29 @@ namespace TCCHD2D.Code
     {
         public bool fought;
 
-        private void Awake()
-        {
-            var infoReader = QuickSaveReader.Create("ItemInfo");
-            var saveReader = QuickSaveReader.Create("GameSave");
-            
-            if (saveReader.Exists(name+"_BossEncountered") && saveReader.Read<bool>(name+"_BossEncountered"))
-            {
-                if (fought)
-                    Destroy(gameObject);
-            }
-            else if (infoReader.Exists(name+"_BossEncountered") && infoReader.Read<bool>(name+"_BossEncountered"))
-            {
-                if (fought)
-                    Destroy(gameObject);
-            }
-        }
-        
         public void FightStarted()
         {
             fought = true;
-            QuickSaveWriter.Create("ItemInfo").Write(name+"_BossEncountered", true).Commit();
+            QuickSaveWriter.Create("ItemInfo").Write(name + "_BossEncountered", true).Commit();
+        }
+
+        public void Check()
+        {
+            var infoReader = QuickSaveReader.Create("ItemInfo");
+            var saveReader = QuickSaveReader.Create("GameSave");
+
+            if (saveReader.Exists(name + "_BossEncountered"))
+            {
+                saveReader.Read<bool>(name + "_BossEncountered");
+                if (fought)
+                    Destroy(gameObject);
+            }
+            if (infoReader.Exists(name + "_BossEncountered"))
+            {
+                fought = infoReader.Read<bool>(name + "_BossEncountered");
+                if (fought)
+                    Destroy(gameObject);
+            }
         }
     }
 }
